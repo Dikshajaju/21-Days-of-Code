1eb8e55e6a20259d34267594f99a9a61feb36182
Diksha Jaju <dikshajaju2022@gmail.com>
2026-06-16 20:08:23 +0530

    Add guessing game that prompts user for a number

---
+++ Day-02/guessing_game.py
@@ -0,0 +1,14 @@
+import random
+
+secret_number = random.randint(1, 100)
+
+while True:
+    guess = int(input("Guess a number between 1 and 100: "))
+
+    if guess == secret_number:
+        print("Congratulations! You guessed it.")
+        break
+    elif guess < secret_number:
+        print("Too Low!")
+    else:
+        print("Too High!")
