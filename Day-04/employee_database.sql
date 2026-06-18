fb95ed8d2d0e3acccc87488f6439169b2238bd45
Diksha Jaju <dikshajaju2022@gmail.com>
2026-06-18 23:08:27 +0530

    Create employee database and table with sample data

--- Day-04/employee_database.sql
+++ Day-04/employee_database.sql
@@ -1,1 +1,26 @@
+create database users;
+use users;
 
+CREATE TABLE employees(
+  emp_id INT,
+  name VARCHAR(50),
+  department VARCHAR(50),
+  salary INT
+);
+
+INSERT INTO employees VALUES
+(1, 'Amit','IT',23000),
+(2, 'sumit','HR',35000),
+(3, 'Diyaa','IT',50000),
+(4, 'Anisha','Comp',30000);
+
+
+SELECT *  
+FROM employees;
+where salary > 30000;
+
+SELECT *  
+FROM employees;
+where department = 'IT';
+
+SELECT MAX(salary) FROM employees;
